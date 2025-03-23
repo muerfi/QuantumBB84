2093cd39208460f553946587376fbf28eb85be94
mrphy <204506223+muerfi@users.noreply.github.com>
2025-03-23 07:04:56 +0100

    Create model1.py

---
+++ Docs/Equation/scripts/model1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+attack_types = ["No Eve", "Intercept-Resend", "Coherent"]
+error_rates = [0, 0.25, 0.15]
+
+plt.bar(attack_types, error_rates, color=['green', 'red', 'blue'])
+plt.xlabel("Attack Type")
+plt.ylabel("Error Rate")
+plt.title("Error Rates in BB84 Simulations")
+plt.savefig("C:/Users/Neqti/Downloads/figure1.png")
+plt.show()
